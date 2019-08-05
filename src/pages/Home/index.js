fb2576c1a1a7c3024b57ea4e8e70fddf0124f128
Adriano Biolchi <adrianobiolchi@outlook.com>
2019-08-04 23:05:52 -0300

    create home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-masculino/86/D16-2255-086/D16-2255-086_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-masculino/86/D16-2255-086/D16-2255-086_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-masculino/86/D16-2255-086/D16-2255-086_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-masculino/86/D16-2255-086/D16-2255-086_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-masculino/86/D16-2255-086/D16-2255-086_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-mizuno-jet-3-n-masculino/86/D16-2255-086/D16-2255-086_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
